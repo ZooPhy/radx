e11d40345c83318ce4e87c6ad4ba2e422dee7c5b
Arjun Magge <amagge@tank.local>
2021-08-31 16:46:44 -0400

    update template

--- radx/settings_template.py
+++ radx/settings_template.py
@@ -3,9 +3,20 @@
 # Copy settings_template.py into a new file and rename the new file to to settings.py  
 # Do not commit settings.py to GitHub repository as it may contain passwords
 
-# Important paths
+### We use the Selenium library to automatically download GISAID FASTA and metadata files
+SELENIUM_BROWSER = "chrome"
+
+### GISAID username and password
+GISAID_USERNAME = ""
+GISAID_PASSWORD = ""
+
+### Important paths
+# path where reference sequences (fasta, primers etc.) are held
 PATH_TO_REFS = "resources/refs/"
+# path where GISAID sequences are stored
+PATH_TO_DOWNLOADS = ""
 PATH_TO_GISAID = "resources/gisaid/"
+# path where local fastq.gz files are stored
 PATH_TO_SRA = "resources/sra/"
-
-
+# path local parent working directory
+PATH_TO_JOBS = "resources/jobs/"
