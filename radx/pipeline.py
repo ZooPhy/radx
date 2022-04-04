97b10d3d7fb11fbcbade41b04ae2208c9fa64996
RafaSFontenele <rafasfontenele@gmail.com>
2022-04-04 18:38:43 -0400

    galaxy pipeline changes

--- radx/pipeline.py
+++ radx/pipeline.py
@@ -10,8 +10,7 @@ from os import chdir, listdir, makedirs, remove, removedirs, system
 from os.path import exists, isdir, isfile, join
 
 from radx.settings import PATH_TO_HOSTING, PATH_TO_REFS, PATH_TO_AGGREGATE
-from radx.utils import (read_ivar, read_lofreq, merge_calls,
-                        filter_merged_calls)
+from radx.utils import (read_lofreq, completemask)
 
 class SRAProcess(Process):
     def __init__(self, fname, in_dir, out_dir, overwrite=False, queue=None):
@@ -54,6 +53,7 @@ class SRAProcess(Process):
         self.ref_fa = "NC_045512.2.fa"
         self.ref_gff = "NC_045512.2.gff"
         self.primer_bed = "swift_primers.bed"
+        self.primer_info = "swif_primers_info.tsv"
         # some of the important output files to indicate pipeline processing
         self.sort_bam = self.name+".sorted.bam"
         self.trim_sort_bam = self.name+".trimmed.sorted.bam"
@@ -87,7 +87,6 @@ class SRAProcess(Process):
         self.sort_bai = self.name+".sorted.bai"
         self.trim = self.name+".trimmed"
         self.trim_bam = self.name+".trimmed.bam"
-        self.final_ivar = self.name + "_ivar.tsv"
         self.sort_dep = self.name+".sorted.depth"
         self.trim_sort_dep = self.name+".trimmed.sorted.depth"
         self.trim_sort_bai = self.name+".trimmed.sorted.bam.bai"
@@ -95,8 +94,19 @@ class SRAProcess(Process):
         self.out_r2 = self.name+"_R2.fastq"
         
         self.trim_sort_indelqual = self.name+"_trim_sort_indelqual"
-        self.final_lofreq = self.name+"_lofreq.vcf"
-        self.variants_merged = self.name+"_variants_merged.tsv"
+        self.trim_sort_indelqual_readsremoved = self.name+"_trim_sort_indelqual_readsremoved"
+        self.lofreq_1 = self.name+"_lofreq_1.vcf"
+        self.lofreq_1_filtered = self.name+"_lofreq_1_filtered.vcf"
+        self.lofreq_2 = self.name+"_lofreq_2.vcf"
+        self.intersect = self.name+"_intersect.vcf"
+        self.union = self.name+"_union.vcf"
+        self.union_filtered = self.name+"_union_filtered.vcf"
+        self.union_snpEff = self.name+"_union_snpEff.vcf"
+        self.union_snpEff_tsv = self.name+"_union_snpEff.tsv"
+        self.intersect_renamed = self.name+"_intersect_renamed.vcf"
+        self.primer_mismatch_indices = self.name+"_primer_mismatch_indices"
+        self.primer_mismatch_indices_v2 = self.name+"_primer_mismatch_indices_v2"
+        #self.variants_merged = self.name+"_variants_merged.tsv"
         self.stats = self.name+".stats"
         self.plot_dir = "plots"
         self.freyja_variants = self.name+"_freyja_variants.tsv"
@@ -107,50 +117,57 @@ class SRAProcess(Process):
         if not exists(self.sort_bam) or self.overwrite:
             # first align to reference sequence
             self.run_cmd(["bwa", "index", self.ref_fa])
-            self.run_cmd(["bwa", "mem", "-t", "32", self.ref_fa, self.sra_r1, self.sra_r2,
-                        "|", "samtools", "view", "-b", "-F", "4",
-                        "|", "samtools","sort", "-o", self.sort_bam])
+            self.run_cmd(["bwa", "mem", "-t", "32","-v","1", self.ref_fa, self.sra_r1, self.sra_r2,
+                        "|", "samtools", "view", "-@", "32","-b", "-f", "1", "-F", "268", "-q", "20", "-s","1.0",
+                        "|", "samtools","sort", "-@", "32", "-o", self.sort_bam])
 
     def trim_to_bam(self):
         if not exists(self.trim_sort_bam) or self.overwrite:
             #trimming primers and base quality
-            self.run_cmd(["ivar", "trim", "-b", self.primer_bed, "-p", self.trim, "-i", self.sort_bam])
-            #checking trimmed vs non trimmed
-            self.run_cmd(["samtools", "sort", "-o", self.trim_sort_bam, self.trim_bam])
+            self.run_cmd(["ivar", "trim", "-e", "-m", "1", "-q", "0", "-b", self.primer_bed, "-p", self.trim, "-i", self.sort_bam])
+            #realign with lofreq
+            self.run_cmd(["lofreq", "viterbi", "--defqual", "2", "-f", self.ref_fa, self.trim, "|"
+            "samtools", "sort", "-@", "32", "-o", self.trim_sort_bam])
             # index the sortedbam file
+
+    def remove_reads(self):
+        self.run_cmd(["lofreq", "indelqual", "--dindel", "-f", self.ref_fa, self.trim_sort_bam, "-o", self.trim_sort_indelqual])
+        self.run_cmd(["lofreq", "call", "--min-cov", "5", "--max-depth", "1000000", "-q", "30", "-Q", "30", "-e", "--min-mq", "20", "--sig", "0.0005", "--bonf", "dynamic",
+         "--no-default-filter", "--no-default-filter", "-f", self.ref_fa, "--call-indels", "-o", self.lofreq_1])
+        self.run_cmd(["lofreq", "filter", "-V", "0", "-v", "5", "-a", "0.05", "-A", "0.95", "-i", self.lofreq_1, "-o", self.lofreq_1_filtered])
+        self.run_cmd(["ivar", "getmasked", "-i", self.lofreq_1_filtered, "-b", self.primer_bed, "-f", self.primer_info, "-p", self.primer_mismatch_indices])
+        complete_mask = completemask(self.primer_mismatch_indices, self.primer_info)
+        with open(self.primer_mismatch_indices_v2, 'w') as o:
+            o.write(complete_mask + '\n')
+        self.run_cmd(["ivar", "removereads", "-i", self.trim_sort_indelqual, "-b", self.primer_bed, "-t", self.primer_mismatch_indices_v2, "-p", self.trim_sort_indelqual_readsremoved]) # final bam file
         if not exists(self.trim_sort_bai) or self.overwrite:
-            self.run_cmd(["samtools", "index", self.trim_sort_bam])
+            self.run_cmd(["samtools", "index", self.trim_sort_indelqual_readsremoved])
             # get depth of the trimmed and sorted bam file for later
         if not exists(self.trim_sort_dep) or self.overwrite:
-            self.run_cmd(["samtools", "depth", "-a", self.trim_sort_bam, ">", self.trim_sort_dep])
+            self.run_cmd(["samtools", "depth", "-a", self.trim_sort_indelqual_readsremoved, ">", self.trim_sort_dep])
             # convert to fastq.gz for uploads
         if not exists(self.out_r1) or not exists(self.out_r2) or self.overwrite:
-            self.run_cmd(["bedtools", "bamtofastq", "-i", self.trim_sort_bam, "-fq", self.out_r1, "-fq2", self.out_r2])
+            self.run_cmd(["bedtools", "bamtofastq", "-i", self.trim_sort_indelqual_readsremoved, "-fq", self.out_r1, "-fq2", self.out_r2])
 
     def variants(self):
-        # Run ivar to get variants
-        if not exists(self.final_ivar) or self.overwrite:
-            self.run_cmd(["samtools", "mpileup", "-aa", "-A", "-B", "-d", "0", "--reference " +self.ref_fa+ " -Q", "0", self.trim_sort_bam,
-                          "|", "ivar", "variants", "-p", self.final_ivar, "-t", "0", "-q", "20", "-m", "10", "-r", self.ref_fa, "-g", self.ref_gff])
         # Run lofreq to get variants
-        if not exists(self.final_lofreq) or self.overwrite:
-            self.run_cmd(["lofreq", "indelqual", "--dindel", "-f", self.ref_fa, self.trim_sort_bam, "-o", self.trim_sort_indelqual])
-            self.run_cmd(["lofreq", "call", "-f", self.ref_fa, "--call-indels", "-o", self.final_lofreq, self.trim_sort_indelqual])
+        self.run_cmd(["lofreq", "call", "--min-cov", "5", "--max-depth", "1000000", "-q", "30", "-Q", "30", "-e", "--min-mq", "20", "--sig", "0.0005", "--bonf", "dynamic",
+         "--no-default-filter", "--no-default-filter", "-f", self.ref_fa, "--call-indels", "-o", self.lofreq_2, self.trim_sort_indelqual_readsremoved])
+        self.run_cmd(["vcfintersect", "-r", self.ref_fa, "-v", "-w", "0", "-i", self.lofreq_2, self.lofreq_1_filtered, ">", self.intersect])
+        self.run_cmd(["sed", "-r", "--sandbox", "-e", 's/^(#CHROM.+)$/##FILTER=<ID=AmpliconRemoval,Description="Variant removed upon removal of amplicon">\n\1/g', "-e", 's/(.+\t)PASS(\t.+)/\1AmpliconRemoval\2/g', self.intersect > self.intersect_renamed])
+        self.run_cmd(["vcfintersect", "-r", self.ref_fa, "-w", "0", "-u", self.intersect_renamed, self.lofreq_2, ">", self.union])
+        self.run_cmd(["lofreq", "filter", "-V", "0", "-v", "0", "-a", "0.0", "-A", "0.0", "-b", "fdr", "-c", "0.001", "--print-all", "-i", self.union, "-o", self.union_filtered])
+        self.run_cmd(["snpEff", "eff", "-nodownload", "-dataDir", "/data", "-i", "vcf", "-o", "vcf", "-formatEff", "-classic", "-no-downstream", "-no-intergenic", "-no-upstream", "-ud", "0", "-stats", "stats.html", "-noLog", "NC_045512.2", self.union_filtered, ">", self.union_snpEff])
         # variants are recorded in metrics, so don't run this if it exists
         if not exists(self.metrics) or self.overwrite:
-            # Read ivar and lofreq
-            ivar_calls = read_ivar(self.final_ivar) if exists(self.final_ivar) else pd.DataFrame()
-            if len(ivar_calls.index) == 0:
-                logging.warning("Empty ivar dataframe :%s", self.final_ivar)
-            lofreq_calls = read_lofreq(self.final_lofreq) if exists(self.final_lofreq) else pd.DataFrame()
+            # Read vcf lofreq final
+            lofreq_calls = read_lofreq(self.union_snpEff) if exists(self.union_snpEff) else pd.DataFrame()
             if len(lofreq_calls.index) == 0:
                 logging.warning("Empty lofreq dataframe :%s", self.final_lofreq)
-            merged_calls = merge_calls(ivar_calls, lofreq_calls)
-            filtered_merged_calls = filter_merged_calls(merged_calls)
-            filtered_merged_calls.to_csv(self.variants_merged, sep="\t", index=False)
+            lofreq_calls.to_csv( self.union_snpEff_tsv, sep="\t", index=False)
             with open("variants.csv", "w") as ofile:
-                if "Variant" in filtered_merged_calls.columns.tolist():
-                    print(",".join(filtered_merged_calls["Variant"].tolist()), end="", file=ofile)
+                if "Variant" in lofreq_calls.columns.tolist():
+                    print(",".join(lofreq_calls["Variant"].tolist()), end="", file=ofile)
                 else:
                     print("", file=ofile)
 
@@ -236,16 +253,16 @@ class SRAProcess(Process):
                             self.trim_sort_bam,
                             self.trim_sort_bai,
                             self.trim_sort_dep,
+                            self.trim_sort_indelqual_readsremoved,
                             self.out_r1,
                             self.out_r2,
                             self.plot_dir,
                             self.stats,
                             self.log_path,
                             self.std_out,
-                            self.metrics,                   # metrics file
-                            self.final_ivar,                # variants from ivar
-                            self.final_lofreq,              # variants from lofreq 
-                            self.variants_merged,            # merged variants
+                            self.metrics,                     # metrics file
+                            self.union_snpEff,                  # variants from lofreq vcf
+                            self.union_snpEff_tsv,              # variants from lofreq final tsv
                             self.freyja_variants,
                             self.freyja_summary
                         ]
--- radx/utils.py
+++ radx/utils.py
@@ -17,36 +17,91 @@ GENE_MAP = {'ORF1a': [266, 13468],
             'N': [28274, 29533],
             'ORF10': [29558, 29674]}
 
-def read_ivar(filename):
-    ivar_calls = pd.read_csv(filename, sep='\t')
-    if ivar_calls.empty:
-        return ivar_calls
-    ivar_calls["Variant"] = ivar_calls.apply(lambda row:
-                                    np.nan if row["ALT"][0] == "-" else \
-                                    (str(row["POS"]) +  ":" + str(row["ALT"][1:]) \
-                                     if row["ALT"][0] == "+" else \
-                                     str(row["REF"]) + str(row["POS"]) +  str(row["ALT"])), axis=1)
-    ivar_calls = ivar_calls.dropna(subset=["Variant"]).drop_duplicates(subset=["Variant"])
-    ivar_calls["SOURCE"] = "ivar"
-    return ivar_calls
+# def read_ivar(filename):
+#     ivar_calls = pd.read_csv(filename, sep='\t')
+#     if ivar_calls.empty:
+#         return ivar_calls
+#     ivar_calls["Variant"] = ivar_calls.apply(lambda row:
+#                                     np.nan if row["ALT"][0] == "-" else \
+#                                     (str(row["POS"]) +  ":" + str(row["ALT"][1:]) \
+#                                      if row["ALT"][0] == "+" else \
+#                                      str(row["REF"]) + str(row["POS"]) +  str(row["ALT"])), axis=1)
+#     ivar_calls = ivar_calls.dropna(subset=["Variant"]).drop_duplicates(subset=["Variant"])
+#     ivar_calls["SOURCE"] = "ivar"
+#     return ivar_calls
 
 def read_lofreq(filename):
-    lofreq_calls = pd.DataFrame(columns=["REGION", "POS", "REF", "ALT", "QUAL", 
+    lofreq_calls = pd.DataFrame(columns=["REGION", "POS", "REF", "ALT", "QUAL", "FILTER", 
                                          "REF_DP", "REF_RV", "ALT_DP", "ALT_RV",
-                                         "ALT_FREQ", "TOTAL_DP"])
+                                         "ALT_FREQ", "TOTAL_DP", "STRAND-BIAS", "EFFECT"])
     vcf_reader = vcf.Reader(filename=filename)
     for row in vcf_reader:
-        lofreq_calls = lofreq_calls.append({"REGION": row.CHROM, 
+        if len(row.FILTER) == 0 and "EFF" not in row.INFO:
+            lofreq_calls = lofreq_calls.append({"REGION": row.CHROM, 
                                             "POS": int(row.POS),
                                             "REF": str(row.REF),
                                             "ALT": str(row.ALT[0]),
                                             "QUAL": row.QUAL, 
+                                            "FILTER": "",
                                             "REF_DP": row.INFO["DP4"][0] + row.INFO["DP4"][1], 
                                             "REF_RV": row.INFO["DP4"][1],
                                             "ALT_DP": row.INFO["DP4"][2] + row.INFO["DP4"][3],
                                             "ALT_RV": row.INFO["DP4"][3],
                                             "ALT_FREQ": row.INFO["AF"],
                                             "TOTAL_DP": row.INFO["DP"],
+                                            "STRAND-BIAS":row.INFO["SB"],
+                                            "EFFECT":""
+                                           }, 
+                                           ignore_index=True)
+        if len(row.FILTER) > 0 and "EFF" in row.INFO:
+            lofreq_calls = lofreq_calls.append({"REGION": row.CHROM, 
+                                            "POS": int(row.POS),
+                                            "REF": str(row.REF),
+                                            "ALT": str(row.ALT[0]),
+                                            "QUAL": row.QUAL, 
+                                            "FILTER": row.FILTER[0],
+                                            "REF_DP": row.INFO["DP4"][0] + row.INFO["DP4"][1], 
+                                            "REF_RV": row.INFO["DP4"][1],
+                                            "ALT_DP": row.INFO["DP4"][2] + row.INFO["DP4"][3],
+                                            "ALT_RV": row.INFO["DP4"][3],
+                                            "ALT_FREQ": row.INFO["AF"],
+                                            "TOTAL_DP": row.INFO["DP"],
+                                            "STRAND-BIAS":row.INFO["SB"],
+                                            "EFFECT":row.INFO["EFF"]
+                                           }, 
+                                           ignore_index=True)
+        if len(row.FILTER) == 0 and "EFF" in row.INFO:
+            lofreq_calls = lofreq_calls.append({"REGION": row.CHROM, 
+                                            "POS": int(row.POS),
+                                            "REF": str(row.REF),
+                                            "ALT": str(row.ALT[0]),
+                                            "QUAL": row.QUAL, 
+                                            "FILTER": "",
+                                            "REF_DP": row.INFO["DP4"][0] + row.INFO["DP4"][1], 
+                                            "REF_RV": row.INFO["DP4"][1],
+                                            "ALT_DP": row.INFO["DP4"][2] + row.INFO["DP4"][3],
+                                            "ALT_RV": row.INFO["DP4"][3],
+                                            "ALT_FREQ": row.INFO["AF"],
+                                            "TOTAL_DP": row.INFO["DP"],
+                                            "STRAND-BIAS":row.INFO["SB"],
+                                            "EFFECT":row.INFO["EFF"]
+                                           }, 
+                                           ignore_index=True)
+        if len(row.FILTER) > 0 and "EFF" not in row.INFO:
+            lofreq_calls = lofreq_calls.append({"REGION": row.CHROM, 
+                                            "POS": int(row.POS),
+                                            "REF": str(row.REF),
+                                            "ALT": str(row.ALT[0]),
+                                            "QUAL": row.QUAL, 
+                                            "FILTER": row.FILTER[0],
+                                            "REF_DP": row.INFO["DP4"][0] + row.INFO["DP4"][1], 
+                                            "REF_RV": row.INFO["DP4"][1],
+                                            "ALT_DP": row.INFO["DP4"][2] + row.INFO["DP4"][3],
+                                            "ALT_RV": row.INFO["DP4"][3],
+                                            "ALT_FREQ": row.INFO["AF"],
+                                            "TOTAL_DP": row.INFO["DP"],
+                                            "STRAND-BIAS":row.INFO["SB"],
+                                            "EFFECT":""
                                            }, 
                                            ignore_index=True)
     if lofreq_calls.empty:
@@ -58,44 +113,65 @@ def read_lofreq(filename):
                                          str(row["POS"] + len(row["REF"][1:]) + 1) if len(row["REF"]) > 1 else \
                                          str(row["REF"]) + str(row["POS"]) + \
                                          str(row["ALT"])), axis=1)
-    lofreq_calls["SOURCE"] = "lofreq"
+    #lofreq_calls = lofreq_calls[lofreq_calls["FILTER"] != "AmpliconRemoval"]
     return lofreq_calls
 
-def annotate_mutations(row):
-    mutations = []
-    gene = next((gene for gene, (first, last) in GENE_MAP.items() if first <= row["POS"] <= last), "")
-    if gene != "" and row["PASS"] == True and "+" not in row["ALT"]:
-        aa_pos = int((row["POS"] - GENE_MAP[gene][0]) // 3 + 1)
-        mutations.append(f"{gene}:{row['REF_AA']}{str(aa_pos)}{row['ALT_AA']}")
-    return mutations
-
-def merge_calls(ivar, lofreq):
-    if ivar.empty:
-        return ivar
-    elif lofreq.empty:
-        return lofreq
-    merged = pd.concat([lofreq, ivar[ivar['ALT_FREQ'] > 0.01]], ignore_index=True)
-    # We want to keep mutations that are common in both ivar and lofreq
-    ivar_snps = set([x for x in merged[merged["SOURCE"]=="ivar"]["Variant"] if "-" not in x and ":" not in x])
-    lofreq_snps = set([x for x in merged[merged["SOURCE"]=="lofreq"]["Variant"] if "-" not in x and ":" not in x])
-    # remove mutations that were not found in both ivar and lofreq
-    outer_snps = ivar_snps.symmetric_difference(lofreq_snps)
-    before = len(merged.index)
-    merged = merged[~merged['Variant'].isin(outer_snps)]
-    logging.info("Removed %s symmetric difference mutations on ivar and lofreq", (before-len(merged.index)))
-    # from redundant mutations, keeps the ivar information for amino acid anotation
-    merged = merged.drop_duplicates(subset=["Variant"], keep='last')
-    merged = merged.sort_values(by=["POS"])
-    if merged.empty:
-        return merged
-    merged["Mutation"] = merged.apply(lambda x: annotate_mutations(x), axis=1)
-    return merged
-
-def filter_merged_calls(merged, min_af=0.05):
-    if "ALT_FREQ" in merged:
-        filtered_merged = merged[~((merged["ALT_FREQ"] < min_af))]
-        if "PASS" in filtered_merged:
-            filtered_merged = filtered_merged[~filtered_merged['PASS'].isin([False])]
-        return filtered_merged
+def completemask(file, primerinfo):
+    with open(file) as i:
+        getmasked_output = i.readline().strip()
+    if not getmasked_output:
+        print()
+        print('No affected primer binding sites found!')
     else:
-        return merged
+        masked_primers = getmasked_output.split('\t')
+        with open(primerinfo) as i:
+            amplicon_data = [line.strip().split('\t') for line in i]
+        masked_complete = []
+        for primer in masked_primers:
+            for amplicon in amplicon_data:
+                if primer in amplicon:
+                    masked_complete += amplicon
+        result = '\t'.join(sorted(set(masked_complete)))
+        print()
+        print('Removing reads primed with any of:')
+    return result
+
+
+# def annotate_mutations(row):
+#     mutations = []
+#     gene = next((gene for gene, (first, last) in GENE_MAP.items() if first <= row["POS"] <= last), "")
+#     if gene != "" and row["PASS"] == True and "+" not in row["ALT"]:
+#         aa_pos = int((row["POS"] - GENE_MAP[gene][0]) // 3 + 1)
+#         mutations.append(f"{gene}:{row['REF_AA']}{str(aa_pos)}{row['ALT_AA']}")
+#     return mutations
+
+# def merge_calls(ivar, lofreq):
+#     if ivar.empty:
+#         return ivar
+#     elif lofreq.empty:
+#         return lofreq
+#     merged = pd.concat([lofreq, ivar[ivar['ALT_FREQ'] > 0.01]], ignore_index=True)
+#     # We want to keep mutations that are common in both ivar and lofreq
+#     ivar_snps = set([x for x in merged[merged["SOURCE"]=="ivar"]["Variant"] if "-" not in x and ":" not in x])
+#     lofreq_snps = set([x for x in merged[merged["SOURCE"]=="lofreq"]["Variant"] if "-" not in x and ":" not in x])
+#     # remove mutations that were not found in both ivar and lofreq
+#     outer_snps = ivar_snps.symmetric_difference(lofreq_snps)
+#     before = len(merged.index)
+#     merged = merged[~merged['Variant'].isin(outer_snps)]
+#     logging.info("Removed %s symmetric difference mutations on ivar and lofreq", (before-len(merged.index)))
+#     # from redundant mutations, keeps the ivar information for amino acid anotation
+#     merged = merged.drop_duplicates(subset=["Variant"], keep='last')
+#     merged = merged.sort_values(by=["POS"])
+#     if merged.empty:
+#         return merged
+#     merged["Mutation"] = merged.apply(lambda x: annotate_mutations(x), axis=1)
+#     return merged
+
+# def filter_merged_calls(merged, min_af=0.05):
+#     if "ALT_FREQ" in merged:
+#         filtered_merged = merged[~((merged["ALT_FREQ"] < min_af))]
+#         if "PASS" in filtered_merged:
+#             filtered_merged = filtered_merged[~filtered_merged['PASS'].isin([False])]
+#         return filtered_merged
+#     else:
+#         return merged
