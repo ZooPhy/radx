610a4793169c7220b083d97affdbf6b4a97e46dc
Arjun Magge <amagge@tank.local>
2022-05-25 15:05:23 -0400

    intermediate changes

--- radx/pipeline.py
+++ radx/pipeline.py
@@ -32,7 +32,11 @@ class SRAProcess(Process):
         # Process the input files
         self.prep()
         self.align()
-        self.trim_to_bam()
+        self.trim_realign()
+        self.indelqual_call_filter()
+        self.get_mismatched_indices()
+        self.remove_reads()
+        self.create_fastq_for_upload()
         self.variants()
         # Analyze files
         self.collect_metrics()
@@ -52,21 +56,26 @@ class SRAProcess(Process):
         self.sra_r2 = self.name+"_R2.fastq.gz"
         self.ref_fa = "NC_045512.2.fa"
         self.ref_gff = "NC_045512.2.gff"
-        self.primer_bed = "swift_primers.bed"
-        self.primer_info = "swif_primers_info.tsv"
+        self.primer_bed = "swift_primers_v2.bed"
+        self.primer_info = "swift_primers_v2_info.tsv"
         # some of the important output files to indicate pipeline processing
-        self.sort_bam = self.name+".sorted.bam"
-        self.trim_sort_bam = self.name+".trimmed.sorted.bam"
+        self.sort_bam = self.name+"_sort.bam"
+        self.sort_trim_bam = self.name+"_sort_trim.bam"
+        self.sort_trim_ra_bam = self.name+"_sort_trim_ra.bam"
+        self.sort_trim_ra_iq_bam = self.name+"_sort_trim_ra_iq.bam"
+        self.sort_trim_ra_iq_rr_bam = self.name+"_sort_trim_ra_iq_rr.bam"
+        self.final_bam = self.sort_trim_ra_iq_rr_bam
         # move files if not fully processed or they don't exist already
-        if not exists(join(self.sdir, self.trim_sort_bam)) or self.overwrite:
+        if not exists(join(self.sdir, self.final_bam)) or self.overwrite:
             for sfile in [self.sra_r1, self.sra_r2]:
                 if not isfile(join(self.sdir, sfile)):
                     self.run_cmd(["cp", join(self.in_dir, sfile), self.sdir])
         # TODO: verify if the following are needed for each record
         # or if they can be just done once and referred across SRA jobs
-        for sfile in [self.ref_fa, self.ref_gff, self.primer_bed]:
+        for sfile in [self.ref_fa, self.ref_gff, self.primer_bed, self.primer_info]:
             if not isfile(join(self.sdir, sfile)):
                 self.run_cmd(["cp", join(PATH_TO_REFS, sfile), self.sdir])
+
         # Update paths to job specific directory
         if isdir("radx"):
             chdir(self.sdir)
@@ -78,23 +87,39 @@ class SRAProcess(Process):
                             filemode=('w' if self.overwrite else 'a'), filename=self.log_path,
                             level=logging.DEBUG)
         logging.info("Current dir contents: %s", " ".join(listdir()))
+
         # Verify if all source files exist
         if self.overwrite:
             assert all([isfile(x) for x in [self.sra_r1, self.sra_r2]])
             assert all([isfile(x) for x in [self.ref_fa, self.ref_gff]])
             assert all([isfile(x) for x in [self.primer_bed]])
-        # Variables intermediate and result files
-        self.sort_bai = self.name+".sorted.bai"
-        self.trim = self.name+".trimmed"
-        self.trim_bam = self.name+".trimmed.bam"
-        self.sort_dep = self.name+".sorted.depth"
-        self.trim_sort_dep = self.name+".trimmed.sorted.depth"
-        self.trim_sort_bai = self.name+".trimmed.sorted.bam.bai"
+
+        # Variables for intermediate and result files
+        self.sort_bai = self.name+"_sort.bam.bai"
+        self.sort_dep = self.name+"_sort.bam.depth"
+
+        self.sort_trim_bai = self.name+"_sort_trim.bam.bai"
+        self.sort_trim_dep = self.name+"_sort_trim.bam.depth"
+
+        self.sort_trim_ra_bai = self.name+"_sort_trim_ra.bam.bai"
+        self.sort_trim_ra_dep = self.name+"_sort_trim_ra.bam.depth"
+
+        self.trim_sort_ra_iq_bai = self.name+"_trim_sort_ra_iq.bam.bai"
+        self.trim_sort_ra_iq_dep = self.name+"_trim_sort_ra_iq.bam.depth"
+
+        # Create copy of local variables for desginating final bam file
+        self.trim_sort_ra_iq_rr_bai = self.name+"_trim_sort_ra_iq_rr.bam.bai"
+        self.trim_sort_ra_iq_rr_dep = self.name+"_trim_sort_ra_iq_rr.bam.depth"
+
+        # Desginating final bam file for variant extraction
+        self.final_bai = self.trim_sort_ra_iq_rr_bai
+        self.final_dep = self.trim_sort_ra_iq_rr_dep
+
+        # Final files for uploading to SRA
         self.out_r1 = self.name+"_R1.fastq"
         self.out_r2 = self.name+"_R2.fastq"
-        
-        self.trim_sort_indelqual = self.name+"_trim_sort_indelqual"
-        self.trim_sort_indelqual_readsremoved = self.name+"_trim_sort_indelqual_readsremoved"
+
+        # Intermediate files for variant extraction
         self.lofreq_1 = self.name+"_lofreq_1.vcf"
         self.lofreq_1_filtered = self.name+"_lofreq_1_filtered.vcf"
         self.lofreq_2 = self.name+"_lofreq_2.vcf"
@@ -104,8 +129,8 @@ class SRAProcess(Process):
         self.union_snpEff = self.name+"_union_snpEff.vcf"
         self.union_snpEff_tsv = self.name+"_union_snpEff.tsv"
         self.intersect_renamed = self.name+"_intersect_renamed.vcf"
-        self.primer_mismatch_indices = self.name+"_primer_mismatch_indices"
-        self.primer_mismatch_indices_v2 = self.name+"_primer_mismatch_indices_v2"
+        self.primer_mismatch_indices = self.name+"_primer_mismatch_indices.txt"
+        self.primer_mismatch_indices_v2 = self.name+"_primer_mismatch_indices_v2.txt"
         #self.variants_merged = self.name+"_variants_merged.tsv"
         self.stats = self.name+".stats"
         self.plot_dir = "plots"
@@ -121,49 +146,73 @@ class SRAProcess(Process):
                         "|", "samtools", "view", "-@", "32","-b", "-f", "1", "-F", "268", "-q", "20", "-s","1.0",
                         "|", "samtools","sort", "-@", "32", "-o", self.sort_bam])
 
-    def trim_to_bam(self):
-        if not exists(self.trim_sort_bam) or self.overwrite:
-            #trimming primers and base quality
-            self.run_cmd(["ivar", "trim", "-e", "-m", "1", "-q", "0", "-b", self.primer_bed, "-p", self.trim, "-i", self.sort_bam])
+    def trim_realign(self):
+        if not exists(self.sort_trim_bam) or self.overwrite:
+            # trimming primers and base quality
+            # TODO: Some flags may need to be updated
+            self.run_cmd(["ivar", "trim", "-e", "-m", "1", "-q", "0", "-b", self.primer_bed, "-p", self.sort_trim_bam, "-i", self.sort_bam])
+        if not exists(self.sort_trim_ra_bam) or self.overwrite:
             #realign with lofreq
-            self.run_cmd(["lofreq", "viterbi", "--defqual", "2", "-f", self.ref_fa, self.trim, "|"
-            "samtools", "sort", "-@", "32", "-o", self.trim_sort_bam])
-            # index the sortedbam file
+            # TODO: check if we need to index the sorted trimmed bam file
+            self.run_cmd(["lofreq", "viterbi", "--defqual", "2", "-f", self.ref_fa, self.sort_trim_bam, "|"
+                          "samtools", "sort", "-@", "32", "-o", self.sort_trim_ra_bam])
+
+    def indelqual_call_filter(self):
+        if (exists(self.sort_trim_ra_bam) and not exists(self.sort_trim_ra_iq_bam)) or self.overwrite:
+            # indelqual
+            self.run_cmd(["lofreq", "indelqual", "--dindel", "-f", self.ref_fa, self.sort_trim_ra_bam, "-o", self.sort_trim_ra_iq_bam])
+        if (exists(self.sort_trim_ra_iq_bam) and not exists(self.lofreq_1)) or self.overwrite:
+            # call indels
+            self.run_cmd(["lofreq", "call", "--min-cov", "5", "--max-depth", "1000000", "-q", "30", "-Q", "30", "-e", "--min-mq", "20", "--sig", "0.0005", "--bonf", "dynamic",
+                          "--no-default-filter", "--no-default-filter", "-f", self.ref_fa, "--call-indels", "-o", self.lofreq_1, self.sort_trim_ra_iq_bam])
+        if (exists(self.lofreq_1) and not exists(self.lofreq_1_filtered)) or self.overwrite:
+            # filter
+            self.run_cmd(["lofreq", "filter", "-V", "0", "-v", "5", "-a", "0.05", "-A", "0.95", "-i", self.lofreq_1, "-o", self.lofreq_1_filtered])
+
+    def get_mismatched_indices(self):
+        if (exists(self.lofreq_1_filtered) and not exists(self.primer_mismatch_indices)) or self.overwrite:
+            # get mask
+            self.run_cmd(["ivar", "getmasked", "-i", self.lofreq_1_filtered, "-b", self.primer_bed, "-f", self.primer_info, "-p", self.primer_mismatch_indices])
+        if (exists(self.primer_mismatch_indices) and not exists(self.primer_mismatch_indices_v2)) or self.overwrite:
+            # write mask to tsv
+            complete_mask = completemask(self.primer_mismatch_indices, self.primer_info)
+            with open(self.primer_mismatch_indices_v2, 'w') as o:
+                o.write(complete_mask + '\n')
 
     def remove_reads(self):
-        self.run_cmd(["lofreq", "indelqual", "--dindel", "-f", self.ref_fa, self.trim_sort_bam, "-o", self.trim_sort_indelqual])
-        self.run_cmd(["lofreq", "call", "--min-cov", "5", "--max-depth", "1000000", "-q", "30", "-Q", "30", "-e", "--min-mq", "20", "--sig", "0.0005", "--bonf", "dynamic",
-         "--no-default-filter", "--no-default-filter", "-f", self.ref_fa, "--call-indels", "-o", self.lofreq_1])
-        self.run_cmd(["lofreq", "filter", "-V", "0", "-v", "5", "-a", "0.05", "-A", "0.95", "-i", self.lofreq_1, "-o", self.lofreq_1_filtered])
-        self.run_cmd(["ivar", "getmasked", "-i", self.lofreq_1_filtered, "-b", self.primer_bed, "-f", self.primer_info, "-p", self.primer_mismatch_indices])
-        complete_mask = completemask(self.primer_mismatch_indices, self.primer_info)
-        with open(self.primer_mismatch_indices_v2, 'w') as o:
-            o.write(complete_mask + '\n')
-        self.run_cmd(["ivar", "removereads", "-i", self.trim_sort_indelqual, "-b", self.primer_bed, "-t", self.primer_mismatch_indices_v2, "-p", self.trim_sort_indelqual_readsremoved]) # final bam file
-        if not exists(self.trim_sort_bai) or self.overwrite:
-            self.run_cmd(["samtools", "index", self.trim_sort_indelqual_readsremoved])
+        if (exists(self.primer_mismatch_indices_v2) and exists(self.sort_trim_ra_iq_bam) and not exists(self.sort_trim_ra_iq_rr_bam)) or self.overwrite:
+            # remove reads based on the mismtatched indices
+            self.run_cmd(["ivar", "removereads", "-i", self.sort_trim_ra_iq_bam, "-b", self.primer_bed, "-t", self.primer_mismatch_indices_v2, "-p", self.sort_trim_ra_iq_rr_bam]) # final bam file
+
+    def create_fastq_for_upload(self):
+        if not exists(self.final_bai) or self.overwrite:
+            # index the final bm file
+            self.run_cmd(["samtools", "index", self.final_bam])
+        if not exists(self.final_dep) or self.overwrite:
             # get depth of the trimmed and sorted bam file for later
-        if not exists(self.trim_sort_dep) or self.overwrite:
-            self.run_cmd(["samtools", "depth", "-a", self.trim_sort_indelqual_readsremoved, ">", self.trim_sort_dep])
-            # convert to fastq.gz for uploads
+            self.run_cmd(["samtools", "depth", "-a", self.final_bam, ">", self.final_dep])
         if not exists(self.out_r1) or not exists(self.out_r2) or self.overwrite:
-            self.run_cmd(["bedtools", "bamtofastq", "-i", self.trim_sort_indelqual_readsremoved, "-fq", self.out_r1, "-fq2", self.out_r2])
+            # convert to fastq.gz for uploads
+            self.run_cmd(["bedtools", "bamtofastq", "-i", self.final_bam, "-fq", self.out_r1, "-fq2", self.out_r2])
 
     def variants(self):
-        # Run lofreq to get variants
+        # Run lofreq, vcfintersect to get variants
         self.run_cmd(["lofreq", "call", "--min-cov", "5", "--max-depth", "1000000", "-q", "30", "-Q", "30", "-e", "--min-mq", "20", "--sig", "0.0005", "--bonf", "dynamic",
-         "--no-default-filter", "--no-default-filter", "-f", self.ref_fa, "--call-indels", "-o", self.lofreq_2, self.trim_sort_indelqual_readsremoved])
+                      "--no-default-filter", "--no-default-filter", "-f", self.ref_fa, "--call-indels", "-o", self.lofreq_2, self.final_bam])
         self.run_cmd(["vcfintersect", "-r", self.ref_fa, "-v", "-w", "0", "-i", self.lofreq_2, self.lofreq_1_filtered, ">", self.intersect])
         self.run_cmd(["sed", "-r", "--sandbox", "-e", 's/^(#CHROM.+)$/##FILTER=<ID=AmpliconRemoval,Description="Variant removed upon removal of amplicon">\n\1/g', "-e", 's/(.+\t)PASS(\t.+)/\1AmpliconRemoval\2/g', self.intersect > self.intersect_renamed])
         self.run_cmd(["vcfintersect", "-r", self.ref_fa, "-w", "0", "-u", self.intersect_renamed, self.lofreq_2, ">", self.union])
         self.run_cmd(["lofreq", "filter", "-V", "0", "-v", "0", "-a", "0.0", "-A", "0.0", "-b", "fdr", "-c", "0.001", "--print-all", "-i", self.union, "-o", self.union_filtered])
+        # files needed for snpEff
+        # wget https://snpeff.blob.core.windows.net/databases/v5_0/snpEff_v5_0_NC_045512.2.zip
+        # unzip snpEff_v5_0_NC_045512.2.zip #will create data folder
         self.run_cmd(["snpEff", "eff", "-nodownload", "-dataDir", "/data", "-i", "vcf", "-o", "vcf", "-formatEff", "-classic", "-no-downstream", "-no-intergenic", "-no-upstream", "-ud", "0", "-stats", "stats.html", "-noLog", "NC_045512.2", self.union_filtered, ">", self.union_snpEff])
         # variants are recorded in metrics, so don't run this if it exists
         if not exists(self.metrics) or self.overwrite:
             # Read vcf lofreq final
             lofreq_calls = read_lofreq(self.union_snpEff) if exists(self.union_snpEff) else pd.DataFrame()
             if len(lofreq_calls.index) == 0:
-                logging.warning("Empty lofreq dataframe :%s", self.final_lofreq)
+                logging.warning("Empty lofreq dataframe :%s", self.lofreq_2)
             lofreq_calls.to_csv( self.union_snpEff_tsv, sep="\t", index=False)
             with open("variants.csv", "w") as ofile:
                 if "Variant" in lofreq_calls.columns.tolist():
@@ -173,17 +222,17 @@ class SRAProcess(Process):
 
     def collect_metrics(self):
         if not exists(self.metrics) or self.overwrite:
-            if not exists(self.trim_sort_bam):
-                logging.warning("Cannot find the bam file :%s . Cannot collect metrics.", self.trim_sort_bam)
+            if not exists(self.final_bam):
+                logging.warning("Cannot find the bam file :%s . Cannot collect metrics.", self.final_bam)
             else:
                 # "Sample", "Breadth of coverage", "Total read count", "Mean reads"
                 breadth, count, mean, variants = "final.bam.breadth", "final.bam.count", "final.bam.mean", "variants.csv"
-                self.run_cmd(["samtools", "depth", "-a", self.trim_sort_bam, "|", 
-                            "awk '{c++; if($3>10) total+=1} END {print total*100/c}'",
-                            ">", breadth], redirect=False)
-                self.run_cmd(["samtools", "view", "-c", "-F", "4", self.trim_sort_bam, ">", count], redirect=False)
-                self.run_cmd(["samtools", "depth", "-a", self.trim_sort_bam, "|", 
-                            "awk '{c++;s+=$3}END{print s/c}'", ">", mean], redirect=False)
+                self.run_cmd(["samtools", "depth", "-a", self.final_bam, "|", 
+                              "awk '{c++; if($3>10) total+=1} END {print total*100/c}'",
+                              ">", breadth], redirect=False)
+                self.run_cmd(["samtools", "view", "-c", "-F", "4", self.final_bam, ">", count], redirect=False)
+                self.run_cmd(["samtools", "depth", "-a", self.final_bam, "|", 
+                              "awk '{c++;s+=$3}END{print s/c}'", ">", mean], redirect=False)
                 if exists(breadth):
                     breadth = [y for y in open(breadth)]
                     breadth = breadth[0].strip() if breadth else "0" 
@@ -208,8 +257,8 @@ class SRAProcess(Process):
                         ofile.write("\t".join([self.name, breadth, count, mean, variants]))
 
     def plot(self):
-        if not exists(self.trim_sort_bam):
-            logging.warning("Cannot find the bam file :%s", self.trim_sort_bam)
+        if not exists(self.final_bam):
+            logging.warning("Cannot find the bam file :%s", self.final_bam)
         else:
             # use samtools to get stats and then plot
             # if not exists(self.stats) or self.overwrite: 
@@ -218,12 +267,12 @@ class SRAProcess(Process):
 
             # run freyja variant generation - this has to be done again for some reason
             if not exists(self.freyja_variants) or self.overwrite:
-                self.run_cmd(["samtools mpileup -aa -A -d 600000 -Q 20 -q 0 -B -f", self.ref_fa, self.trim_sort_bam, "|",
-                              "tee >(cut -f1-4 > "+self.trim_sort_dep+")", "|",
+                self.run_cmd(["samtools mpileup -aa -A -d 600000 -Q 20 -q 0 -B -f", self.ref_fa, self.final_bam, "|",
+                              "tee >(cut -f1-4 > "+self.final_dep+")", "|",
                               "ivar variants -p", self.freyja_variants, "-q 20 -t 0.0 -r", self.ref_fa])
             # run freyja summary
             if not exists(self.freyja_summary) or self.overwrite:
-                self.run_cmd(["freyja", "demix", self.freyja_variants, self.trim_sort_dep, "--output", self.freyja_summary])
+                self.run_cmd(["freyja", "demix", self.freyja_variants, self.final_dep, "--output", self.freyja_summary])
 
     def move_files(self):
         if not PATH_TO_HOSTING.strip():
@@ -250,10 +299,10 @@ class SRAProcess(Process):
         files_to_keep = [
                             self.sort_bam,
                             self.sort_dep,
-                            self.trim_sort_bam,
-                            self.trim_sort_bai,
-                            self.trim_sort_dep,
-                            self.trim_sort_indelqual_readsremoved,
+                            self.sort_trim_bam,
+                            self.final_bam,
+                            self.final_bai,
+                            self.final_dep,
                             self.out_r1,
                             self.out_r2,
                             self.plot_dir,
--- radx/utils.py
+++ radx/utils.py
@@ -17,19 +17,6 @@ GENE_MAP = {'ORF1a': [266, 13468],
             'N': [28274, 29533],
             'ORF10': [29558, 29674]}
 
-# def read_ivar(filename):
-#     ivar_calls = pd.read_csv(filename, sep='\t')
-#     if ivar_calls.empty:
-#         return ivar_calls
-#     ivar_calls["Variant"] = ivar_calls.apply(lambda row:
-#                                     np.nan if row["ALT"][0] == "-" else \
-#                                     (str(row["POS"]) +  ":" + str(row["ALT"][1:]) \
-#                                      if row["ALT"][0] == "+" else \
-#                                      str(row["REF"]) + str(row["POS"]) +  str(row["ALT"])), axis=1)
-#     ivar_calls = ivar_calls.dropna(subset=["Variant"]).drop_duplicates(subset=["Variant"])
-#     ivar_calls["SOURCE"] = "ivar"
-#     return ivar_calls
-
 def read_lofreq(filename):
     lofreq_calls = pd.DataFrame(columns=["REGION", "POS", "REF", "ALT", "QUAL", "FILTER", 
                                          "REF_DP", "REF_RV", "ALT_DP", "ALT_RV",
@@ -120,8 +107,8 @@ def completemask(file, primerinfo):
     with open(file) as i:
         getmasked_output = i.readline().strip()
     if not getmasked_output:
-        print()
-        print('No affected primer binding sites found!')
+        logging.info('No affected primer binding sites found!')
+        result = ""
     else:
         masked_primers = getmasked_output.split('\t')
         with open(primerinfo) as i:
@@ -132,46 +119,6 @@ def completemask(file, primerinfo):
                 if primer in amplicon:
                     masked_complete += amplicon
         result = '\t'.join(sorted(set(masked_complete)))
-        print()
-        print('Removing reads primed with any of:')
+        logging.info('Removing reads primed with any of:')
     return result
 
-
-# def annotate_mutations(row):
-#     mutations = []
-#     gene = next((gene for gene, (first, last) in GENE_MAP.items() if first <= row["POS"] <= last), "")
-#     if gene != "" and row["PASS"] == True and "+" not in row["ALT"]:
-#         aa_pos = int((row["POS"] - GENE_MAP[gene][0]) // 3 + 1)
-#         mutations.append(f"{gene}:{row['REF_AA']}{str(aa_pos)}{row['ALT_AA']}")
-#     return mutations
-
-# def merge_calls(ivar, lofreq):
-#     if ivar.empty:
-#         return ivar
-#     elif lofreq.empty:
-#         return lofreq
-#     merged = pd.concat([lofreq, ivar[ivar['ALT_FREQ'] > 0.01]], ignore_index=True)
-#     # We want to keep mutations that are common in both ivar and lofreq
-#     ivar_snps = set([x for x in merged[merged["SOURCE"]=="ivar"]["Variant"] if "-" not in x and ":" not in x])
-#     lofreq_snps = set([x for x in merged[merged["SOURCE"]=="lofreq"]["Variant"] if "-" not in x and ":" not in x])
-#     # remove mutations that were not found in both ivar and lofreq
-#     outer_snps = ivar_snps.symmetric_difference(lofreq_snps)
-#     before = len(merged.index)
-#     merged = merged[~merged['Variant'].isin(outer_snps)]
-#     logging.info("Removed %s symmetric difference mutations on ivar and lofreq", (before-len(merged.index)))
-#     # from redundant mutations, keeps the ivar information for amino acid anotation
-#     merged = merged.drop_duplicates(subset=["Variant"], keep='last')
-#     merged = merged.sort_values(by=["POS"])
-#     if merged.empty:
-#         return merged
-#     merged["Mutation"] = merged.apply(lambda x: annotate_mutations(x), axis=1)
-#     return merged
-
-# def filter_merged_calls(merged, min_af=0.05):
-#     if "ALT_FREQ" in merged:
-#         filtered_merged = merged[~((merged["ALT_FREQ"] < min_af))]
-#         if "PASS" in filtered_merged:
-#             filtered_merged = filtered_merged[~filtered_merged['PASS'].isin([False])]
-#         return filtered_merged
-#     else:
-#         return merged
